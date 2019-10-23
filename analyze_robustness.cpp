c144479d1fd2575e682442320019cc718553d66a
Den125 <sasha_isaikin@mail.ru>
2019-10-24 00:13:04 +0700

    изменен анализ описания, в связи с изменением структуры описания, требуются доп.тесты

--- analyze_robustness.cpp
+++ analyze_robustness.cpp
@@ -2,11 +2,12 @@
 #include <QFile>
 #include <QTextStream>
 #include <QJsonDocument>
-#include "analyzing.h"
+#include "analyze.h"
 
 QStringList analyze_robustness_file(QString str);
-QStringList analyze_description_main(QMap<QString,Structures::complex>* list_elements, QString key, QJsonObject main, QStringList objects, ProjectData Project);
-QStringList analyze_description_alt(QMap<QString,Structures::complex>* list_elements, QString key, QJsonObject alt, QStringList object, ProjectData Projects);
+//QStringList analyze_description_main(QMap<QString,Structures::complex>* list_elements, QString key, QJsonObject main, QStringList objects, ProjectData Project);
+//QStringList analyze_description_alt(QMap<QString,Structures::complex>* list_elements, QString key, QJsonObject alt, QStringList object, ProjectData Projects);
+QStringList analyze_description(QMap<QString,Structures::complex>* list_elements, QString key, QJsonObject json_object, QStringList objects, ProjectData Project);
 
 
 QStringList analyze_robustness_file(QString str)
@@ -33,7 +34,7 @@ QStringList analyze_robustness_file(QString str)
     }
     return objects;
 }
-QStringList analyze_description_main(QMap<QString,Structures::complex>* list_elements, QString key, QJsonObject main, QStringList objects, ProjectData Project)
+/*QStringList analyze_description_main(QMap<QString,Structures::complex>* list_elements, QString key, QJsonObject main, QStringList objects, ProjectData Project)
 {
     QString str;
     QStringList sequences;
@@ -131,6 +132,72 @@ QStringList analyze_description_alt(QMap<QString,Structures::complex>* list_elem
     }
     return sequences;
 }
+*/
+QStringList analyze_description(QMap<QString,Structures::complex>* list_elements, QString key, QJsonObject json_object, QStringList objects, ProjectData Project)
+{
+    QStringList sequences;
+    const QString format_text=".txt",format_picture=".png";
+    int index_seq=0;
+    for (;index_seq<json_object.count()-3;index_seq++)
+    {
+        QString str,name;
+        QJsonValue seq_list=json_object.value("list_seq"+QString::number(index_seq));
+        if (seq_list.toObject().value("type_seq").toString()=="main")
+        {
+            str.append("Главная последовательность:\n");
+            name=key+"_main";
+            list_elements->insert(name,{Structures::DiagramType::sequence,true});
+        }
+        else
+        {
+            str.append("Альтернативная последовательность "+QString::number(index_seq)+":\n");
+            name=key+"_alt"+QString::number(index_seq);
+            list_elements->insert(name,{Structures::sequence,true});
+        }
+        int index_elem=0;
+        QJsonValue list_seq=seq_list.toObject().value("seq");
+        if (!(list_seq.toObject().keys().empty()))
+        {
+            for (;index_elem<list_seq.toObject().keys().count();index_elem++)
+            {
+                QJsonValue elem_seq=list_seq.toObject().value(QString::number(index_elem));
+                str.append(QString::number(index_elem)+") "+elem_seq.toString()+"\n");
+            }
+        }
+        sequences<<str;
+        QFile newfile(Project.path+"/"+Structures::type(Structures::sequence)+"/"+name+format_text);
+        if (newfile.open(QIODevice::ReadWrite))
+        {
+            QTextStream text(&newfile);
+            QString file_text;
+            if (newfile.size()!=0)
+            {
+                file_text=text.readAll();
+                QRegExp note("note left.*end note");
+                int index=0;
+                if ((index= note.indexIn(file_text,index))!=-1)
+                {
+                    file_text.replace(index,note.matchedLength()-17,'\n'+str+'\n');
+                    newfile.resize(0);
+                    text<<file_text;
+                }
+                else
+                {
+                    file_text.chop(7);
+                    file_text.append("note left\n "+str+"\n end note");
+                    newfile.resize(0);
+                    text<<file_text<<objects.join(' ')<<"\n@enduml";
+                }
+            }
+            else
+            {
+                text<<"@startuml\nnote left\n"<<str<<"end note\n"<<objects.join('\n')<<"\n@enduml";
+            }
+            newfile.close();
+        }
+    }
+    return sequences;
+ }
 
 QMap<QString,Structures::complex> analyze_robustness_diagram(QString filename, ProjectData Project)
 {
@@ -141,7 +208,8 @@ QMap<QString,Structures::complex> analyze_robustness_diagram(QString filename, P
     QFile file(Project.path+"/"+Structures::type(Structures::robustness)+"/"+filename+format_text);
     if (file.open(QIODevice::ReadOnly))
     {
-        file_str=file.readAll();
+        QTextStream in_file(&file);
+        file_str=in_file.readAll();
         objects=analyze_robustness_file(file_str);
     }
     QFile desc(Project.path+"/"+"description.json");
@@ -156,10 +224,7 @@ QMap<QString,Structures::complex> analyze_robustness_diagram(QString filename, P
                 return list_elements;
             }
             QJsonObject root = doc.object().value(filename).toObject();
-            QJsonObject main=root.value("main_seq").toObject();
-            sequences<<analyze_description_main(&list_elements,filename,main,objects,Project);
-            QJsonObject alt=root.value("alt_seq").toObject();
-            sequences<<analyze_description_alt(&list_elements,filename,alt,objects,Project);
+            sequences<<analyze_description(&list_elements,filename,root,objects,Project);
             desc.close();
         }
         QRegExp note("\nnote as scenario.*end note\n");
